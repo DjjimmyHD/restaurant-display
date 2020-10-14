94efe2fd0f11bb2b92c7b8a23b8693f0667867a6
ninjames101 <ninjames101@gmail.com>
2020-10-14 09:51:57 -0600

    added fetch call, it is working, storing data in state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, {Component} from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-    </div>
-  );
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      restaurants: []
+    };
+  }
+  componentDidMount() {
+    fetch("https://code-challenge.spectrumtoolbox.com/api/restaurants", {
+      headers: {
+        Authorization: "Api-Key q3MNxtfep8Gt",
+      },
+    })
+    .then((response) => response.json())
+      .then((data) => {
+        this.setState({ restaurants: data });
+      });
+  }
+  render() {
+    return (
+      <section>
+        <h1>I am here as a place holder fam</h1>
+      </section>
+    );
+  }
 }
 
 export default App;
